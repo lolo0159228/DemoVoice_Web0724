0573058d7c4954dde1590df56b2a61339870b508
lolo0159228 <40049303+lolo0159228@users.noreply.github.com>
2019-08-13 22:10:48 +0800

    Add files via upload

--- Web0724/home/static/js/audioplayer.js
+++ Web0724/home/static/js/audioplayer.js
@@ -1,14 +1,19 @@
 var currentTrack = null;
-var paused = false;
+var paused = true;
+var audio = null;
+var a = null;
 
 $(document).ready(function(){
-    var a = document.getElementById("currentTrack");
+    a = document.getElementById("currentTrack");
     currentTrack = a.getAttribute("value");
+    audio = "<audio  id='Rock' src='/static/uploads/Sleep_Away.mp3' preload='auto'></audio>";
+    getaudio();
     //currentTrack = $('#currentTrack').val();
 
     $('#play').click(function(){
-        let y = document.getElementById("currentTrack");
-        alert(y.innerHTML);
+        //document.getElementById("play").innerHTML="<audio  id='Rock' src='/static/uploads/Sleep_Away.mp3' preload='auto'></audio>";
+        /*let y = document.getElementById("currentTrack");
+        1alert(y.innerHTML);  test用*/
         play();
     });
     $('#stop').click(function(){
@@ -22,12 +27,21 @@ $(document).ready(function(){
 
 function play(){
     if (pause == false){
-        stop();
+        currentTrack = a.getAttribute("value");
+        $('#icon-audio').addClass(" oi-media-play").removeClass(" oi-media-pause");
+        //document.getElementById('icon-audio').innerHTML+= "<span class='oi oi-media-play'></span>";
        // currentTrack = $('#currentTrack').val();
-       currentTrack = a.getAttribute("value");
-       document.getElementById(currentTrack).load();
+       document.getElementById(currentTrack).pause();
+       pause = true;
+    }else{
+        $('#icon-audio').addClass(" oi-media-pause").removeClass(" oi-media-play");
+        //document.getElementById('icon-audio').innerHTML+=  "<span class='text-center oi oi-media-pause'>";
+        //currentTrack = a.getAttribute("value");
+        //document.getElementById(currentTrack).load(); 重載更新
+        document.getElementById(currentTrack).play();
+        pause = false;
     }
-    document.getElementById(currentTrack).play();
+
 }
 
 function stop(){
@@ -40,5 +54,9 @@ function stop(){
 function pause(){
     paused = true;
     document.getElementById(currentTrack).pause();
+}
+
+function getaudio(){
+    document.getElementById('play').innerHTML+= audio;
 }
 
